1400d550ca9bbda30267cdc5d24ac8d86dde26a4
Jonh Alex <122692601+Jonhvmp@users.noreply.github.com>
2024-12-26 15:01:57 -0300

    :construction: adiciona suporte a usuários e serviços com schemas e métodos de CRUD

---
+++ backend/src/services/schemas/service.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type ServiceDocument = Service & Document;
+
+@Schema()
+export class Service {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ required: true })
+  price: number;
+
+  @Prop({ required: true })
+  providerId: string; // ID do prestador de serviços
+}
+
+export const ServiceSchema = SchemaFactory.createForClass(Service);
